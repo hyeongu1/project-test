db3abbb60017841010b438b539380093880b0e22
hyeongu1 <gusrn556111@gmail.com>
2023-06-15 09:37:37 +0900

    test

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import styled from "styled-components";
+
+const inputbox = styled.button`
+width: 1000px;
+padding: 30px;
+background-color: black;
+`;
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <inputbox>dasd</inputbox>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
